cda72d7d6e3accffd42abac6b9dff80b0186adb9
tzw22 <141603595+tzw22@users.noreply.github.com>
2023-08-20 12:50:16 +0800

    Update cli.js

--- cli.js
+++ cli.js
@@ -16,7 +16,7 @@ const argv = require('yargs')
     .option('uuid', {
         alias: 'u',
         describe: 'Specify the uuid',
-        default: 'd342d11e-d424-4583-b36e-524ab1f0afa4'
+        default: '56547ae7-a5c3-430d-a643-ba756e05115d'
     })
     .help()
     .argv;
