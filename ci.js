263da61d91cefef4ad2d333e2a6095c795088efa
tzw22 <141603595+tzw22@users.noreply.github.com>
2023-08-20 12:50:53 +0800

    Update ci.js

--- ci.js
+++ ci.js
@@ -2,7 +2,7 @@ const { createVLESSServer } = require('./index.js');
 
 // 定义端口和 UUID
 const port = 3001;
-const uuid = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+const uuid = '56547ae7-a5c3-430d-a643-ba756e05115d';
 
 // 调用函数启动 VLESS 服务器
 createVLESSServer(port, uuid);
